624dfb9a20e5bb99a9bdbb235b394c0ea3a59899
CrazyQzq <13186061107@163.com>
2022-04-16 21:31:52 +0800

    注释改动

--- oauth2-demo/src/main/java/com/qq/oauth2/demo/config/JwtEnhanceAccessTokenConverter.java
+++ oauth2-demo/src/main/java/com/qq/oauth2/demo/config/JwtEnhanceAccessTokenConverter.java
@@ -5,9 +5,10 @@ import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConv
 import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
 
 /**
- * @author 公众号：码猿技术专栏
- * 令牌转换器，将身份认证信息转换后存储在令牌内
- */
+ * @Description: 令牌转换器，将身份认证信息转换后存储在令牌内
+ * @Author QinQiang
+ * @Date 2022/4/12
+ **/
 public class JwtEnhanceAccessTokenConverter extends DefaultAccessTokenConverter {
     public JwtEnhanceAccessTokenConverter(UserDetailsService userDetailsService){
         super.setUserTokenConverter(new JwtEnhanceUserAuthenticationConverter(userDetailsService));
