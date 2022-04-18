1c40cbf45ca87061b67f23ca0cfa21052b735661
CrazyQZQ <13186061107@163.com>
2022-04-18 19:14:24 +0800

    用户信息中继，防止fein调用是丢失用户信息

---
+++ common/common-system/src/main/java/com/qq/common/system/interceptor/FeignRequestInterceptor.java
@@ -0,0 +1,45 @@
+package com.qq.common.system.interceptor;
+
+import com.qq.common.core.utils.ServletUtils;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author 公众号：码猿技术专栏
+ * 用于实现令牌信息中继
+ */
+@Component
+public class FeignRequestInterceptor implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate template) {
+        HttpServletRequest httpServletRequest = ServletUtils.getRequest();
+        //获取RequestContextHolder中的信息
+        Map<String, String> headers = getHeaders(httpServletRequest);
+        //放入feign的RequestTemplate中
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            template.header(entry.getKey(), entry.getValue());
+        }
+    }
+
+    /**
+     * 获取原请求头
+     */
+    private Map<String, String> getHeaders(HttpServletRequest request) {
+        Map<String, String> map = new LinkedHashMap<>();
+        Enumeration<String> enumeration = request.getHeaderNames();
+        if (enumeration != null) {
+            while (enumeration.hasMoreElements()) {
+                String key = enumeration.nextElement();
+                String value = request.getHeader(key);
+                map.put(key, value);
+            }
+        }
+        return map;
+    }
+}
