59df64b59fe2602e3118e257474b632ab503b393
CrazyQZQ <13186061107@163.com>
2022-04-18 19:17:04 +0800

    从数据库获取url权限。缓存到redis

---
+++ common/common-system/src/main/java/com/qq/common/system/mapper/SysMenuMapper.java
@@ -0,0 +1,7 @@
+package com.qq.common.system.mapper;
+
+import com.github.yulichang.base.MPJBaseMapper;
+import com.qq.common.system.pojo.SysMenu;
+
+public interface SysMenuMapper extends MPJBaseMapper<SysMenu> {
+}
---
+++ common/common-system/src/main/java/com/qq/common/system/pojo/SysMenu.java
@@ -0,0 +1,211 @@
+package com.qq.common.system.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+
+import java.util.Date;
+import java.io.Serializable;
+
+/**
+ * 菜单权限表(SysMenu)实体类
+ *
+ * @author makejava
+ * @since 2022-04-18 15:24:11
+ */
+public class SysMenu implements Serializable {
+    private static final long serialVersionUID = -66901660498791966L;
+    /**
+    * 菜单ID
+    */
+    @TableId
+    private Long menuId;
+    /**
+    * 菜单名称
+    */
+    private String menuName;
+    /**
+    * 父菜单ID
+    */
+    private Long parentId;
+    /**
+    * 显示顺序
+    */
+    private Integer orderNum;
+    /**
+    * 请求地址
+    */
+    private String url;
+    /**
+    * 打开方式（menuItem页签 menuBlank新窗口）
+    */
+    private String target;
+    /**
+    * 菜单类型（M目录 C菜单 F按钮）
+    */
+    private String menuType;
+    /**
+    * 菜单状态（0显示 1隐藏）
+    */
+    private String visible;
+    /**
+    * 是否刷新（0刷新 1不刷新）
+    */
+    private String isRefresh;
+    /**
+    * 权限标识
+    */
+    private String perms;
+    /**
+    * 菜单图标
+    */
+    private String icon;
+    /**
+    * 创建者
+    */
+    private String createBy;
+    /**
+    * 创建时间
+    */
+    private Date createTime;
+    /**
+    * 更新者
+    */
+    private String updateBy;
+    /**
+    * 更新时间
+    */
+    private Date updateTime;
+    /**
+    * 备注
+    */
+    private String remark;
+
+
+    public Long getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(Long menuId) {
+        this.menuId = menuId;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public Integer getOrderNum() {
+        return orderNum;
+    }
+
+    public void setOrderNum(Integer orderNum) {
+        this.orderNum = orderNum;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public String getMenuType() {
+        return menuType;
+    }
+
+    public void setMenuType(String menuType) {
+        this.menuType = menuType;
+    }
+
+    public String getVisible() {
+        return visible;
+    }
+
+    public void setVisible(String visible) {
+        this.visible = visible;
+    }
+
+    public String getIsRefresh() {
+        return isRefresh;
+    }
+
+    public void setIsRefresh(String isRefresh) {
+        this.isRefresh = isRefresh;
+    }
+
+    public String getPerms() {
+        return perms;
+    }
+
+    public void setPerms(String perms) {
+        this.perms = perms;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+}
