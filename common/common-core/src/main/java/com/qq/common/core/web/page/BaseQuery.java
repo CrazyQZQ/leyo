6c5f12830e39eca2a15ba2509024b1793707c2b6
CrazyQZQ <13186061107@163.com>
2022-05-12 15:03:07 +0800

    查询条件基类

---
+++ common/common-core/src/main/java/com/qq/common/core/web/page/BaseQuery.java
@@ -0,0 +1,19 @@
+package com.qq.common.core.web.page;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author QinQiang
+ * @Date 2022/5/12
+ **/
+@Data
+public class BaseQuery {
+    private Date startTime;
+    private Date endTime;
+    private List<Long> ids;
+    private String keyword;
+}
