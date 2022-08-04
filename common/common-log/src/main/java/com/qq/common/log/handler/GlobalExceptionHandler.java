f42a238627efb47ef2a0163a3a00b813a4221acd
CrazyQZQ <13186061107@163.com>
2022-08-04 14:47:40 +0800

    修改项目名称

--- common/common-log/src/main/java/com/qq/common/log/handler/GlobalExceptionHandler.java
+++ common/common-log/src/main/java/com/qq/common/log/handler/GlobalExceptionHandler.java
@@ -12,8 +12,6 @@ import com.qq.common.redis.service.RedisService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
