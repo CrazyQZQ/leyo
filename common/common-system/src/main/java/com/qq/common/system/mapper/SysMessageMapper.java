1cf26bc2880fe661f3f39861d24c6c558ee0d338
CrazyQZQ <13186061107@163.com>
2022-08-29 20:19:23 +0800

    完善消息推送

---
+++ common/common-system/src/main/java/com/qq/common/system/mapper/SysMessageMapper.java
@@ -0,0 +1,18 @@
+package com.qq.common.system.mapper;
+
+import com.qq.common.system.pojo.SysMessage;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Administrator
+* @description 针对表【sys_message(系统消息表)】的数据库操作Mapper
+* @createDate 2022-08-29 11:04:44
+* @Entity com.qq.common.system.pojo.SysMessage
+*/
+public interface SysMessageMapper extends BaseMapper<SysMessage> {
+
+}
+
+
+
+
